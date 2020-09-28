aadc8ea93fb72a9823018c9af1bb6be8241b4035
Eyal Betzalel <eyalbatzul@gmail.com>
2020-09-28 18:05:17 +0300

    Update train.py

--- train.py
+++ train.py
@@ -42,9 +42,9 @@ def main(args):
 
   ##### Load ImageNet ####
     
-  path_train = "/home/dsi/eyalbetzalel/pytorch-generative-v2/pytorch-generative-v2/imagenet64/train_64x64"
+  path_train = "/home/dsi/eyalbetzalel/pytorch-generative-v2/pytorch-generative-v2/imagenet64/train"
   datasetTrain = datasets.ImageFolder(path_train, transform=transform)
-  path_test = "/home/dsi/eyalbetzalel/pytorch-generative-v2/pytorch-generative-v2/imagenet64/valid_64x64"
+  path_test = "/home/dsi/eyalbetzalel/pytorch-generative-v2/pytorch-generative-v2/imagenet64/test"
   datasetTest = datasets.ImageFolder(path_test, transform=transform)
       
 
