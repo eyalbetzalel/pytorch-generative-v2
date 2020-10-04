b27614be33cea7cbf3e87f7363cd77fd7605409d
Eyal Betzalel <eyalbatzul@gmail.com>
2020-10-04 20:08:30 +0300

    Update trainer.py

--- pytorch_generative/trainer.py
+++ pytorch_generative/trainer.py
@@ -194,7 +194,7 @@ class Trainer:
             print("sampling")
             curr_path = 'sample_' + str(self._epoch) + '.png'
             print(curr_path)
-            sampleTensor=self._model.sample((10, 1, 28, 28))
+            sampleTensor=self._model.sample((10, 3, 64, 64))
             sampleTensor=sampleTensor.cpu()
             cu.imsave(sampleTensor, figsize=(50, 5),filename = curr_path)
             
